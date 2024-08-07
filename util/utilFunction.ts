d508b8446e538b14157baa92d9d2850cae861c15
RUDRANSHFLY <mayamayavir@gmail.com>
2024-08-07 17:06:40 +0530

    formatAmount Fucntion Add

---
+++ util/utilFunction.ts
@@ -0,0 +1,8 @@
+export function formatAmount(amount: number) {
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "INR",
+    minimumFractionDigits: 2,
+  });
+  return formatter.format(amount);
+}
