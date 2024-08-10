88e63b8666a7a48fb91fa4fec2947edb31d769d7
RUDRANSHFLY <mayamayavir@gmail.com>
2024-08-10 06:14:06 +0530

    NavBar and TotlBalanceBox Add

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,42 @@
+import NavBar from "@/components/navbar/NavBar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
+  const headerType = "greeting";
+  const headerTitle = "Welcome";
+  const headerUser = {
+    name: "Ghost",
+  };
+
+  const mainTotalBalanceBox = {
+    mainAccount: [],
+    mainTotalBanks: 5,
+    mainTotalCurrentBalance: 1999,
+  };
+
+  const headerSubTitle =
+    "Access and manage your account and transactions efficiently";
+
   return (
     <div>
-      <h1>Home</h1>
+      <section className={"home"}>
+        <div className={"home-content"}>
+          <header className={"home-header"}>
+            <NavBar
+              type={headerType}
+              tiitle={headerTitle}
+              user={headerUser?.name || "Guest"}
+              subTitle={headerSubTitle}
+            />
+            <TotalBalanceBox
+              account={[]}
+              totalBanks={mainTotalBalanceBox.mainTotalBanks}
+              totalCurrentBalance={mainTotalBalanceBox.mainTotalCurrentBalance}
+            />
+          </header>
+        </div>
+      </section>
     </div>
   );
 };
