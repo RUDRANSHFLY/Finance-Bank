1c68dd3184d52985a620409247c19b9946c03250
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-09 14:59:08 +0530

    New Types Add

--- types/index.d.ts
+++ types/index.d.ts
@@ -12,21 +12,83 @@ declare type Account = {
   shareableId: string;
 };
 
-interface HeaderProps {
+declare type User = {
+  $id: string;
+  email: string;
+  userId: string;
+  dwollaCustomerUrl: string;
+  dwollaCustomerId: string;
+  firstName: string;
+  lastName: string;
+  address1: string;
+  city: string;
+  state: string;
+  postalCode: string;
+  dateOfBirth: string;
+  ssn: string;
+};
+
+/* 
+  ! Transaction Related
+*/
+
+declare type Transaction = {
+  id: string;
+  $id: string;
+  name: string;
+  paymentChannel: string;
+  type: string;
+  accountId: string;
+  amount: number;
+  pending: boolean;
+  category: string;
+  date: string;
+  image: string;
+  $createdAt: string;
+  channel: string;
+  senderBankId: string;
+  receiverBankId: string;
+};
+
+export declare interface HeaderProps {
   type?: string | "title";
   tiitle: string;
   user?: string;
   subTitle: string;
 }
 
-interface TotalBalanceBoxProps {
+export declare interface MobileNavProps {
+  user: User;
+}
+
+export declare interface SideBarProps {
+  user: User;
+}
+
+export declare interface RightSideBarProps {
+  user: User;
+  transactions: Transaction[];
+  banks: Bank[] & Account[];
+}
+
+export declare interface TotalBalanceBoxProps {
   account: [];
   totalBanks: number;
   totalCurrentBalance: number;
 }
 
-declare interface DoughnutChartProps {
+export declare interface DoughnutChartProps {
   accounts: Account[];
 }
 
-export { HeaderProps, TotalBalanceBoxProps, DoughnutChartProps };
+export declare interface RightSideBarProps {
+  user: User;
+  transactions: Transaction[];
+  bank: Bank[] & Account[];
+}
+
+export declare interface CreditCardProps {
+  account: Account;
+  userName: string;
+  showBalance?: boolean;
+}
