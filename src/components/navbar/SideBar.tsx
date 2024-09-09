1b14029b3a3f32036041fb9ffc815bee299980c2
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-09 09:46:50 +0530

    SideBar Comp Add

---
+++ src/components/navbar/SideBar.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import React from "react";
+import { SideBarProps } from "../../../types";
+import Link from "next/link";
+import Image from "next/image";
+import { sideBarLinks } from "../../../constants";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
+const SideBar = ({ user }: SideBarProps) => {
+  const pathName = usePathname();
+
+  return (
+    <section className={"sideBar"}>
+      <nav className={"flex flex-col gap-4 items-baseline"}>
+        <Link
+          href={"/"}
+          className={"flex  mb-12 cursor-pointer items-center gap-2"}
+        >
+          <Image
+            src={"/icons/logo.svg"}
+            width={34}
+            height={34}
+            alt={"logo horizon"}
+            className={"size-[24px] max-xl:size-14"}
+          />
+          <h1 className="sideBar-logo">Horizon</h1>
+        </Link>
+
+        {sideBarLinks.map((item) => {
+          const isActive =
+            pathName === item.route || pathName.startsWith(`${item.route}/`);
+          return (
+            <Link
+              href={item.route}
+              key={item.label}
+              className={cn("sideBar-link", {
+                "bg-bankGradient text-white ": isActive,
+              })}
+            >
+              <div className={"relative size-6"}>
+                <Image
+                  src={item.imgURL}
+                  alt={item.label}
+                  fill
+                  className={cn({
+                    "brightness-[3] invert-0": isActive,
+                  })}
+                />
+              </div>
+              <p className={"sideBar-label"}>{item.label}</p>
+            </Link>
+          );
+        })}
+      </nav>
+    </section>
+  );
+};
+
+export default SideBar;
