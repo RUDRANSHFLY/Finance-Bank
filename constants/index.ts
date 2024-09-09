fd58b380a1a3cd0c6aad7a4d8f00fdd9548336cb
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-09 09:47:22 +0530

    SideBarLinks Add

---
+++ constants/index.ts
@@ -0,0 +1,22 @@
+export const sideBarLinks = [
+  {
+    imgURL: "/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/icons/rupees-circle.svg",
+    route: "/my-banks",
+    label: "My Banks",
+  },
+  {
+    imgURL: "/icons/transaction.svg",
+    route: "/transaction-history",
+    label: "Transaction History",
+  },
+  {
+    imgURL: "/icons/payment.svg",
+    route: "/payment-transfer",
+    label: "Transfer Funds",
+  },
+];
