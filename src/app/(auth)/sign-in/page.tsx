595558965193ca82bf11a2bf082404eabfad517a
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-18 07:09:05 +0530

    Auth Form Add

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,10 +1,11 @@
+import AuthForm from "@/components/auth/AuthForm";
 import React from "react";
 
 const SignIn = () => {
   return (
-    <div>
-      <h1>Sign-In</h1>
-    </div>
+    <section className={"flex-center size-full max-sm:px-6"}>
+      <AuthForm type={"sign-in"} />
+    </section>
   );
 };
 
