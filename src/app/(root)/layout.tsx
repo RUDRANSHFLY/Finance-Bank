3166c27ee9c0018b034dd29a0c790d49eb8d2294
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-18 07:08:27 +0530

    rstertication to homepage Add

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,52 +1,29 @@
+import { getLoggedInUser } from "@/actions/user.actions";
 import MobileNavBar from "@/components/navbar/MobileNavBar";
 import SideBar from "@/components/navbar/SideBar";
 import Image from "next/image";
+import { User } from "../../../types";
+import { redirect } from "next/navigation";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const user: User = await getLoggedInUser();
+
+  if (!user) {
+    redirect("/sign-in");
+  }
+
   return (
     <main className={"h-screen w-full flex font-inter"}>
-      <SideBar
-        user={{
-          $id: "",
-          email: "",
-          userId: "",
-          dwollaCustomerUrl: "",
-          dwollaCustomerId: "",
-          firstName: "",
-          lastName: "",
-          address1: "",
-          city: "",
-          state: "",
-          postalCode: "",
-          dateOfBirth: "",
-          ssn: "",
-        }}
-      />
+      <SideBar user={user} />
       <div className={"flex size-full flex-col"}>
         <div className={"root-layout"}>
           <Image src={"/icons/logo.svg"} alt={"logo"} width={30} height={30} />
           <div>
-            <MobileNavBar
-              user={{
-                $id: "",
-                email: "",
-                userId: "",
-                dwollaCustomerUrl: "",
-                dwollaCustomerId: "",
-                firstName: "",
-                lastName: "",
-                address1: "",
-                city: "",
-                state: "",
-                postalCode: "",
-                dateOfBirth: "",
-                ssn: "",
-              }}
-            />
+            <MobileNavBar user={user} />
           </div>
         </div>
         {children}
