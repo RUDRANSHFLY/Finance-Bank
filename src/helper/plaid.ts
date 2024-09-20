0b977e3f5e5ef82523597355e1a797015ca43d58
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-20 15:30:36 +0530

    Plaid Service Add

---
+++ src/helper/plaid.ts
@@ -0,0 +1,16 @@
+import { headers } from "next/headers";
+import { Configuration, PlaidApi, PlaidEnvironments } from "plaid";
+
+const configuration = new Configuration({
+  basePath: PlaidEnvironments.sandbox,
+  baseOptions: {
+    headers: {
+      PLAID_CLIENT_ID: process.env.PLAID_CLIENT_ID,
+      PLAID_SECRET: process.env.PLAID_SECRET,
+    },
+  },
+});
+
+const plaidClient = new PlaidApi(configuration);
+
+export default plaidClient;
